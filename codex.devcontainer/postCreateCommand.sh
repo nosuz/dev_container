cffceac629745208a2119072d26c0874cce38e2b
Norio Suzuki <norisuzu@gmail.com>
2026-03-18 20:19:29 +0900

    Updated: removed adding user.
    Use default user. But if the host UID is not 1000 and need to write under $HOME, need to rebuild with the same UID as of UID on the host.

--- codex.devcontainer/postCreateCommand.sh
+++ codex.devcontainer/postCreateCommand.sh
@@ -2,9 +2,25 @@
 
 set -eu
 
-if [ "${DEVCONTAINER_MODE:-}" != "dev" ]; then
+# チェック対象のディレクトリとマーカーファイル
+MARKER_FILE="$HOME/.postCreateCommand-done"
+
+if [ -f "$MARKER_FILE" ]; then
+  # マーカーファイルがあれば終了
   exit 0
+elif [ ! -w "$HOME" ]; then
+  # ホームに書き込めない時は、異常終了
+  echo "ホームディレクトリが書き込めません。"
+  exit 1
 fi
+
+# 初期化スクリプトを実行
 
 mkdir -p /workspaces/.codex
 ln -sfn /workspaces/.codex ~/.codex
+
+mkdir -p /workspaces/.claude
+ln -sfn /workspaces/.claude ~/.claude
+
+# 初期化完了を示すマーカーファイルを作成
+touch "$MARKER_FILE"
